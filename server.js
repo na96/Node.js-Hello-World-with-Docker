7d1690e632075288943749b503bd3723f198a280
na96 <36153158+na96@users.noreply.github.com>
2018-02-05 16:07:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0'
 //App
 const app = express();
 app.get('/', (req, res) => {
- res.send('Hallo World\n');
+ res.send('Hallo Welt Iam Docker\n');
 });
 
 app.listen(PORT, HOST);
